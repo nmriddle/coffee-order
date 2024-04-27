46e86c5db29417ca685349613d891ef1478984f2
nmriddle <nmriddle@iu.edu>
2024-04-26 23:02:08 -0400

    forgot to do some stupid thing

--- src/main/java/edu/iu/habahram/coffeeorder/controllers/AuthenticationController.java
+++ src/main/java/edu/iu/habahram/coffeeorder/controllers/AuthenticationController.java
@@ -8,6 +8,7 @@ import edu.iu.habahram.coffeeorder.security.TokenService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +31,9 @@ public class AuthenticationController {
     @PostMapping("/signup")
     public void signup(@RequestBody Customer customer) {
         try {
+            BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+            String passwordEncoded = bc.encode(customer.getPassword());
+            customer.setPassword(passwordEncoded);
             customerRepository.save(customer);
         } catch (Exception e) {
             throw new RuntimeException(e);
